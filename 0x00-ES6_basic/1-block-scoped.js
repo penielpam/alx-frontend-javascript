6fdbd56b075df38ef4c78382aacc20f1396f2bd3
Peniel Pam <117779595+penielpam@users.noreply.github.com>
2024-04-26 15:22:46 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // This `task` is scoped only within the if block
-    let task2 = false; // This `task2` is scoped only within the if block
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
