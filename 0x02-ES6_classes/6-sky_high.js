ba1460dcd7217d63c53fee5343ed244d4691a28e
Peniel Pam <117779595+penielpam@users.noreply.github.com>
2024-04-28 18:05:20 +0100

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,18 +3,15 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
